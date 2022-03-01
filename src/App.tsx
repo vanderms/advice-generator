2923c8c30367272ce2b1cdb5be74de3046b29aae
vanderms <vanderms.84@gmail.com>
2022-03-01 11:12:20 -0300

    remove react starter content and add project boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss';
+import PatternDesktop from './assets/pattern-divider-desktop.svg';
+import PatternMobile from './assets/pattern-divider-mobile.svg';
+import Dice from './assets/icon-dice.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={classes.app}>
+      <article className={classes.adviceCard}>
+        <div className={classes.cardContainer}>
+          <h1 className={classes.adviceTitle}>ADVICE #<span>117</span></h1>
+          <p className={classes.advice}>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p>
+          <picture>
+            <source srcSet={PatternMobile} media="(max-width: 600px)"/>
+            <img src={PatternDesktop} alt=""/>
+          </picture>
+          <button className={classes.button} aria-label='next advice'>
+            <img src={Dice} alt=""/>
+          </button>
+        </div>
+      </article>
+    </main>
   );
 }
 
