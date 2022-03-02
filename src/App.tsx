f3b9c60322e8da12e3c180b0a49e3aaad5d47967
vanderms <vanderms.84@gmail.com>
2022-03-02 00:15:12 -0300

    remove default value and add loader

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,56 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-const DEFAULT_ADVICE = {
-  id: 117,
-  advice: "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+interface Advice {
+  id: number | string
+  advice: string
 }
-type Advice = typeof DEFAULT_ADVICE;
+
+async function fetchData() : Promise<Advice> {
+  const url = `https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`;
+  const response = await axios.get(url);
+  const advice: Advice = {
+    id: response.data.slip.id,
+    advice: response.data.slip.advice
+  };
+  return advice;
+}
 
 function App() {
 
-  const [ advice, setAdvice ] = useState<Advice>(DEFAULT_ADVICE);
-  const [ update, setUpdate ] = useState<number>(0);
+  const [ advice, setAdvice ] = useState<Advice>({id: '', advice: ''});
+ 
+  useEffect(() => {
+    setTimeout(getNewAdvice, 500);
+  }, []);
 
-  useEffect(()=> {
-    if(update !== 0){
-      axios.get(`https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`)
-        .then( response => {
-          const newAdvice: Advice = {
-            id: response.data.slip.id,
-            advice: response.data.slip.advice
-          };        
-          setAdvice(newAdvice);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-    }
-  }, [update]);
+  function getNewAdvice(){
+    fetchData()
+    .then( response => setAdvice(response))
+    .catch( err => console.log(err.message));
+  }
+
+  const cardStyle = advice.advice === '' ? 
+    `${classes.adviceCard}  ${classes.loading}` : classes.adviceCard;
+
+  const appStyle = advice.advice === '' ?
+    `${classes.app} ${classes.loading}` : classes.app;
 
 
-  return (
-    <main className={classes.app}>
-      <article className={classes.adviceCard}>      
+  return ( 
+    <main className={appStyle}>
+      <article className={cardStyle}>      
           <h1 className={classes.adviceTitle}>ADVICE #{advice.id}</h1>
           <p className={classes.advice}>"{advice.advice}"</p>
           <picture className={classes.sep}>
             <source srcSet={PatternMobile} media="(max-width: 600px)"/>
             <img src={PatternDesktop} alt=""/>
           </picture>
-          <button className={classes.button} onClick={() => setUpdate(update + 1)} aria-label='next advice'>
+          <button className={classes.button} onClick={getNewAdvice} aria-label='next advice'>
             <img src={Dice} alt=""/>
           </button>        
       </article>
-    </main>
+    </main>    
   );
 }
 
